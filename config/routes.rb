3ee39abc9793c975f29e3849354ce447d7167024
kenniz <kennizchan@gmail.com>
2011-11-18 21:41:12 +0800

    Add configuration folder for rubymine. Add scaffold cart. Add method in application controller to get cart from session.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Depot::Application.routes.draw do
+  resources :carts
+
   get "store/index"
 
   resources :products
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111114145405) do
+ActiveRecord::Schema.define(:version => 20111118132434) do
+
+  create_table "carts", :force => true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "products", :force => true do |t|
     t.string   "title"
