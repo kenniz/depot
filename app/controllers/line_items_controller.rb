3568438af12d461a04fbba0b0d5ac04b8a2547f5
kenniz <kennizchan@gmail.com>
2011-11-28 03:37:00 +0800

    Creating an ajax-based cart

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -47,9 +47,11 @@ class LineItemsController < ApplicationController
     respond_to do |format|
       if @line_item.save
         format.html { redirect_to(store_url) }
+        format.js
         format.json { render json: @line_item, status: :created, location: @line_item }
       else
         format.html { render action: "new" }
+        format.js {render action: "new" }
         format.json { render json: @line_item.errors, status: :unprocessable_entity }
       end
     end
