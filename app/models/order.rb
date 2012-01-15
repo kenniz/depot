fa4c405b743cc9ed601777183c19b4e03d25272b
kenniz <kennizchan@gmail.com>
2012-01-15 10:33:35 +0800

    Data validation and test fixture for order and line items

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,6 @@
 class Order < ActiveRecord::Base
   PAYMENT_TYPES = [ "Check", "Credit card", "Purchase order" ]
+
+  validates :name, :address, :email, :pay_type, :presence => true
+  validates :pay_type, :inclusion => PAYMENT_TYPES
 end
