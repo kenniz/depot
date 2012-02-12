c5d45fbffab9a6a381eb31e52d9ef26e97728c32
kenniz <kennizchan@gmail.com>
2012-02-12 14:30:22 +0800

    Add integration test for buying a book

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -55,7 +55,7 @@ class OrdersController < ApplicationController
         Cart.destroy(session[:cart_id])
         session[:cart_id] = nil
         Notifier.order_received(@order).deliver
-        Notifier.order_shipped(@order).deliver
+        #Notifier.order_shipped(@order).deliver
         format.html { redirect_to(store_url, :notice => 'Thank you for your order.') }
         format.json { render json: @order, status: :created, location: @order }
       else
