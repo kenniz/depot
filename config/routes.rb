075084031598f5766c220ab2dcb5f8e4d1ce71d9
kenniz <kennizchan@gmail.com>
2012-02-27 10:46:30 +0800

    Adding users

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Depot::Application.routes.draw do
+  resources :users
+
   resources :orders
 
   resources :line_items
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120103151028) do
+ActiveRecord::Schema.define(:version => 20120227014730) do
 
   create_table "carts", :force => true do |t|
     t.datetime "created_at"
@@ -41,6 +41,14 @@ ActiveRecord::Schema.define(:version => 20120103151028) do
     t.text     "description"
     t.string   "image_url"
     t.decimal  "price",       :precision => 8, :scale => 2
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "name"
+    t.string   "hashed_password"
+    t.string   "salt"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
