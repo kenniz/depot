b21819551d0fe9bdb8fe6d9d120748b869e0118b
kenniz <kennizchan@gmail.com>
2012-02-27 14:50:48 +0800

    Selecting Locale

--- config/routes.rb
+++ config/routes.rb
@@ -8,18 +8,15 @@ Depot::Application.routes.draw do
     delete 'logout' => :destroy
   end
 
-  resources :users
-
-  resources :orders
-
-  resources :line_items
-
-  resources :carts
-
-  get "store/index"
-
-  resources :products do
-    get :who_bought, :on => :member
+  scope '(:locale)' do
+    resources :users
+    resources :orders
+    resources :line_items
+    resources :carts
+    resources :products do
+      get :who_bought, :on => :member
+    end
+    root :to => 'store#index', :as => 'store'
   end
 
   # The priority is based upon order of creation:
@@ -72,7 +69,6 @@ Depot::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => 'welcome#index'
-  root :to => 'store#index', :as => 'store'
 
   # See how all your routes lay out with "rake routes"
 
