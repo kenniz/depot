1d2d9d5376bdbb05144abaaace0004ec203b3ac0
kenniz <kennizchan@gmail.com>
2012-02-29 11:31:42 +0800

    Add log tunning

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -35,6 +35,7 @@ Depot::Application.configure do
 
   # Use a different logger for distributed setups
   # config.logger = SyslogLogger.new
+  config.logger = Logger.new(config.paths.log.first, 'daily')
 
   # Use a different cache store in production
   # config.cache_store = :mem_cache_store
