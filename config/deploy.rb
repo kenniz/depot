d95d8e22213fced5abb471e46fb7870bcfec0b07
kenniz <kennizchan@gmail.com>
2012-02-29 21:38:13 +0800

    Add the RVM's path environment to capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,3 +1,12 @@
+# Add RVM's lib directory to the load path.
+$:.unshift(File.expand_path('./lib', ENV['rvm_path']))
+
+# Load RVM's capistrano plugin.
+require "rvm/capistrano"
+
+set :rvm_ruby_string, '1.9.2'
+set :rvm_type, :user
+
 # be sure to change these
 set :user, 'kenniz'
 set :domain, 'localhost'
